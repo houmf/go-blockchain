5b77e6a991138492fe8281229abfed2d0c3b04b6
George Antoniadis <george@noodles.gr>
2017-07-19 20:28:01 +0100

    Add mining

--- api.go
+++ api.go
@@ -6,7 +6,8 @@ import (
 )
 
 type API struct {
-	Blocks map[string]*Block // map[parent_hash]*Block
+	Blocks      map[string]*Block // map[parent_hash]*Block
+	LatestBlock *Block
 }
 
 func (a *API) Get(ctx context.Context, block *Block) (*Blocks, error) {
@@ -28,8 +29,10 @@ func (a *API) Announce(ctx context.Context, block *Block) (*Empty, error) {
 	if a.blockExists(block) == true {
 		return nil, nil
 	}
+	// TODO Check if new block is valid
 	phs := fmt.Sprintf("%x", block.ParentHash)
 	a.Blocks[phs] = block
+	a.LatestBlock = block
 	return nil, nil
 }
 
@@ -40,4 +43,23 @@ func (a *API) blockExists(block *Block) bool {
 		}
 	}
 	return false
+}
+
+func (a *API) Mine() {
+	for {
+		currentLatestBlock := a.LatestBlock
+		nonce, diff := CalcNonce(currentLatestBlock)
+		fmt.Println("Found new block nonce=%x with difficulty=%d", nonce, diff)
+		// if the latest block is still the same
+		if string(currentLatestBlock.Hash) == string(a.LatestBlock) {
+			nb := &Block{
+				Data:       []byte{},
+				Nonce:      nonce,
+				ParentHash: currentLatestBlock.Hash,
+			}
+			// TODO serialize nb
+			// TODO nb.Hash = serialized nb
+			// TODO announce block to other peers
+		}
+	}
 }
